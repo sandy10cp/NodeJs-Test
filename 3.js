7e452142e305d70c24bcdeb5ebbf3f89240d6772
sandy10cp <sandy10cp@gmail.com>
2020-08-08 20:16:34 +0700

    edit

--- 3.js
+++ 3.js
@@ -3,8 +3,20 @@ function cekVocal() {
     var v = 'a,i,u,e,o';
     v = v.split(',')
     kata = kata.split('')
-    kata.map(kata => {
-        console.log(kata);
+    kata.map((kata, index) => {
+        if (kata == v[1]) {
+            console.log('$');
+        } else if (kata == v[4]) {
+            console.log('#');
+        } else if (kata == v[3]) {
+            console.log('$');
+        }
+        else if (kata == v[0]) {
+            console.log('#');
+        }
+        else {
+            console.log(kata);
+        }
     })
 }
 cekVocal()
